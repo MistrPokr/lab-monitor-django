4f8e49ddb3a9373493e4100e56d435af5a8d6638
pokr <mistrpokr@outlook.com>
2021-06-01 11:30:12 +0100

    feat:add list view for VideoModels

--- video_tracker/urls.py
+++ video_tracker/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path
-from .views import scan_new_video
+from .views import scan_new_video, list_video
 
 urlpatterns = [
     path("scan/", scan_new_video),
+    path("list/", list_video),
 ]
--- video_tracker/views.py
+++ video_tracker/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 from lab_monitor.settings import VIDEO_STORAGE
 from .models import VideoModel
 from .utils import scan_directory_file
+
+
+@api_view(["GET"])
+def list_video(request):
+    if request.method == "GET":
+        queryset = VideoModel.objects.all()
+        return JsonResponse(list(queryset.values_list('name')), safe=False)
+
 
 
 @api_view(["GET"])
