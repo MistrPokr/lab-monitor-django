44b37283f8fa4463e4bec19fcf65c8657f56619d
pokr <mistrpokr@outlook.com>
2021-06-01 16:05:42 +0100

    feat:add video download api

--- video_tracker/urls.py
+++ video_tracker/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path
-from .views import list_video
+from .views import list_video, download_video
 
 urlpatterns = [
     path("list/", list_video),
+    path("download/<filename>", download_video),
 ]
--- video_tracker/views.py
+++ video_tracker/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, FileResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -12,4 +12,17 @@ from .utils import scan_directory_file
 def list_video(request):
     if request.method == "GET":
         queryset = VideoModel.objects.all()
-        return JsonResponse(list(queryset.values_list("name")), safe=False)
+        name_list = list(queryset.values_list("name"))
+        return JsonResponse([{"name": _[0]} for _ in name_list], safe=False)
+
+
+@api_view(["GET"])
+def download_video(request, filename):
+    if request.method == "GET":
+        target_obj = VideoModel.objects.get(name=filename)
+
+        file_handle = open(target_obj.file, "rb")
+        # file_handle.close()
+
+        response = FileResponse(file_handle, as_attachment=True)
+        return response
