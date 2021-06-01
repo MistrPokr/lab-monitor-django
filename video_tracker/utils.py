dca955dd37e3b6d3091a817164d2930f7786f3d7
pokr <mistrpokr@outlook.com>
2021-06-01 12:16:48 +0100

    feat:add 'phantom' file entry remover, manage utils

--- video_tracker/utils.py
+++ video_tracker/utils.py
@@ -1,7 +1,24 @@
 import os
+from .models import VideoModel
+from lab_monitor.settings import VIDEO_STORAGE
 
 
-def scan_directory_file(directory, extension=".mp4"):
+def scan_new_video(directory=VIDEO_STORAGE):
+    queryset = VideoModel.objects.all()
+    new_file_list = scan_directory_file(directory=directory)
+
+    new_files = set([list(_)[0] for _ in queryset.values_list("name")]) ^ set(
+        list(new_file_list)
+    )
+
+    if new_files is not None:
+        for f in new_files:
+            file_path = VIDEO_STORAGE + f
+            new_vm = VideoModel(name=f, file=file_path)
+            new_vm.save()
+
+
+def scan_directory_file(directory=VIDEO_STORAGE, extension=".mp4"):
     target_list = {}
 
     with os.scandir(directory) as dir_contents:
@@ -10,3 +27,18 @@ def scan_directory_file(directory, extension=".mp4"):
                 target_list[entry.name] = os.stat(entry).st_mtime
 
     return target_list
+
+
+def remove_phantom_files(directory=VIDEO_STORAGE):
+    phantom_list = []
+
+    db_list = [_[0] for _ in VideoModel.objects.all().values_list("name")]
+
+    with os.scandir(directory) as dir_contents:
+        existing_list = [_.name for _ in dir_contents]
+        for _ in db_list:
+            if _ not in existing_list:
+                phantom_list.append(_)
+
+    phantoms_query = VideoModel.objects.filter(name__in=phantom_list)
+    phantoms_query.delete()
