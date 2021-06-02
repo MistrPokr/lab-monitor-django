e51ba49746cf158c97a744f3646f3d6364a6a493
pokr <mistrpokr@outlook.com>
2021-06-03 01:39:26 +0800

    feat(wip):add scan command

---
+++ video_tracker/management/commands/scan.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand, CommandError
+from ...utils import scan_new_video_files, remove_phantom_files
+
+
+class Command(BaseCommand):
+    help = "Instantly run a video scan job"
+
+    def handle(self, *args, **options):
+        scan_new_video_files()
+        remove_phantom_files()
+        self.stdout.write(self.style.SUCCESS("COMMAND HANDLER REGISTERED"))
