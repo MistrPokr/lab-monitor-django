7449f49a8d40b6bfb25c3a45dc150c0af9a33fe9
pokr <mistrpokr@outlook.com>
2021-06-04 05:50:49 +0800

    fix:change media root;add voice file admin

--- voice_handler/admin.py
+++ voice_handler/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import VoiceFile
 
 # Register your models here.
+class VoiceAdmin(admin.ModelAdmin):
+    fields = (
+        "text",
+        "voice_file",
+    )
+
+
+admin.site.register(VoiceFile, VoiceAdmin)
