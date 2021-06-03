0a8e15bd8f01c559a5f5b298f42657fc0830a8a1
pokr <mistrpokr@outlook.com>
2021-06-03 15:21:00 +0800

    feat:add DELETE api for voice file

--- voice_handler/urls.py
+++ voice_handler/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     re_path(r"^upload/", views.file_upload_handler),
     re_path(r"^tts/", views.tts_handler),
     re_path(r"^list/", views.voice_list_handler),
-    re_path(r"^play/(?P<pk>[0-9]+)/$", views.play_voice_handler)
+    re_path(r"^file/(?P<pk>[0-9]+)/$", views.voice_file_handler), 
 ]
